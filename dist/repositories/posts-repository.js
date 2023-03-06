bb2d67d8207473839a3672a0becdaeca93cf7492
pawelsusha <pawelsusha@gmail.com>
2023-03-06 21:23:21 +0100

    + 06/03 async adding

--- dist/repositories/posts-repository.js
+++ dist/repositories/posts-repository.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.postsRepository = exports.posts = void 0;
 exports.posts = [
@@ -21,7 +30,9 @@ exports.posts = [
 ];
 exports.postsRepository = {
     returnAllPosts() {
-        return exports.posts;
+        return __awaiter(this, void 0, void 0, function* () {
+            return exports.posts;
+        });
     },
     /*    postsRepository = {
         findPosts(title: string | null | undefined) {
@@ -33,47 +44,55 @@ exports.postsRepository = {
             }
         },*/
     getPostById(id) {
-        let post = exports.posts.find(p => p.id === id);
-        if (post) {
-            post.id = id;
-            return post;
-        }
-        else {
-            return false;
-        }
+        return __awaiter(this, void 0, void 0, function* () {
+            let post = exports.posts.find(p => p.id === id);
+            if (post) {
+                post.id = id;
+                return post;
+            }
+            else {
+                return false;
+            }
+        });
     },
     createPost(post, blogId, blogName) {
-        const newPost = {
-            id: new Date().toISOString(),
-            title: post.title,
-            shortDescription: post.shortDescription,
-            content: post.content,
-            blogId: blogId,
-            blogName: blogName,
-            // createdAt:new Date().toISOString()
-        };
-        exports.posts.push(newPost);
-        return newPost;
+        return __awaiter(this, void 0, void 0, function* () {
+            const newPost = {
+                id: new Date().toISOString(),
+                title: post.title,
+                shortDescription: post.shortDescription,
+                content: post.content,
+                blogId: blogId,
+                blogName: blogName,
+                // createdAt:new Date().toISOString()
+            };
+            exports.posts.push(newPost);
+            return newPost;
+        });
     },
     updatePost(id, body) {
-        let post = exports.posts.find(p => p.id === id);
-        if (post) {
-            post.title = body.title;
-            post.shortDescription = body.shortDescription;
-            post.content = body.content;
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    deletePost(id) {
-        for (let i = 0; i < exports.posts.length; i++) {
-            if (exports.posts[i].id === id) {
-                exports.posts.splice(i, 1);
+        return __awaiter(this, void 0, void 0, function* () {
+            let post = exports.posts.find(p => p.id === id);
+            if (post) {
+                post.title = body.title;
+                post.shortDescription = body.shortDescription;
+                post.content = body.content;
                 return true;
             }
-        }
-        return false;
+            else {
+                return false;
+            }
+        });
+    },
+    deletePost(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            for (let i = 0; i < exports.posts.length; i++) {
+                if (exports.posts[i].id === id) {
+                    exports.posts.splice(i, 1);
+                    return true;
+                }
+            }
+            return false;
+        });
     }
 };
--- dist/routes/posts-router.js
+++ dist/routes/posts-router.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.adminAuth = exports.basicAuth = exports.postsRouter = void 0;
 const express_1 = require("express");
@@ -17,14 +26,14 @@ exports.adminAuth = (0, exports.basicAuth)({ users: { 'admin': 'qwerty' } });
     res.status(200).send(foundPosts)
 })*/
 //Get All Posts By no auth
-exports.postsRouter.get('/', (req, res) => {
-    let allPosts = posts_repository_1.postsRepository.returnAllPosts();
+exports.postsRouter.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const allPosts = yield posts_repository_1.postsRepository.returnAllPosts();
     res.status(200).send(allPosts);
     return;
-});
+}));
 //Get Post By ID no Auth
-exports.postsRouter.get('/:id', (req, res) => {
-    let post = posts_repository_1.postsRepository.getPostById(req.params.id);
+exports.postsRouter.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let post = yield posts_repository_1.postsRepository.getPostById(req.params.id);
     if (post) {
         res.status(200).send(post);
         return;
@@ -33,36 +42,36 @@ exports.postsRouter.get('/:id', (req, res) => {
         res.sendStatus(404);
         return;
     }
-});
+}));
 //Create Post  + Auth
-exports.postsRouter.post('/', exports.adminAuth, InputValidationMiddleWare_1.postValidationMiddleware, InputValidationMiddleWare_2.inputValidationMiddleware, (req, res) => {
+exports.postsRouter.post('/', exports.adminAuth, InputValidationMiddleWare_1.postValidationMiddleware, InputValidationMiddleWare_2.inputValidationMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(req.body);
     const blog = blogs_repository_1.blogsRepository.getBlogsById(req.body.blogId);
     if (!blog) {
         res.sendStatus(404);
         return;
     }
-    const newPost = posts_repository_1.postsRepository.createPost(req.body, blog.id, blog.name);
+    const newPost = yield posts_repository_1.postsRepository.createPost(req.body, blog.id, blog.name);
     res.status(201).send(newPost);
     return;
-});
+}));
 //Update Post By ID + Auth
-exports.postsRouter.put('/:id', exports.adminAuth, InputValidationMiddleWare_1.postValidationMiddleware, InputValidationMiddleWare_2.inputValidationMiddleware, (req, res) => {
-    const isUpdated = posts_repository_1.postsRepository.updatePost(req.params.id, req.body);
+exports.postsRouter.put('/:id', exports.adminAuth, InputValidationMiddleWare_1.postValidationMiddleware, InputValidationMiddleWare_2.inputValidationMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const isUpdated = yield posts_repository_1.postsRepository.updatePost(req.params.id, req.body);
     if (isUpdated) {
         res.sendStatus(204);
     }
     else {
         res.sendStatus(404);
     }
-});
+}));
 //Delete Post By ID + Auth
-exports.postsRouter.delete('/:id', exports.adminAuth, (req, res) => {
-    const isDeleted = posts_repository_1.postsRepository.deletePost(req.params.id);
+exports.postsRouter.delete('/:id', exports.adminAuth, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const isDeleted = yield posts_repository_1.postsRepository.deletePost(req.params.id);
     if (isDeleted) {
         res.sendStatus(204);
     }
     else {
         res.sendStatus(404);
     }
-});
+}));
