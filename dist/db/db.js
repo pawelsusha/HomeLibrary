7e4c311b427902214c8180bbdd8b1c07227a6227
pawelsusha <pawelsusha@gmail.com>
2023-03-13 15:29:40 +0100

    versel blabla try

--- dist/db/db.js
+++ dist/db/db.js
@@ -16,8 +16,13 @@ exports.runDb = exports.client = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 const mongodb_1 = require("mongodb");
 dotenv_1.default.config();
-//const mongoURI = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017'
-const mongoURI = "mongodb://0.0.0.0:27017/?maxPoolSize=20&w=majority";
+const mongoURI = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017';
+//const mongoURI = "mongodb://0.0.0.0:27017/?maxPoolSize=20&w=majority";
+const url = process.env.MONGO_URL;
+console.log('url :', url);
+if (!url) {
+    throw new Error('❌! Url doesnt found');
+}
 exports.client = new mongodb_1.MongoClient(mongoURI);
 const runDb = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,17 @@
 import dotenv from 'dotenv'
 import {MongoClient} from "mongodb";
 dotenv.config()
-//const mongoURI = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017'
-const mongoURI = "mongodb://0.0.0.0:27017/?maxPoolSize=20&w=majority";
+const mongoURI = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017'
+//const mongoURI = "mongodb://0.0.0.0:27017/?maxPoolSize=20&w=majority";
+
+
+
+const url = process.env.MONGO_URL
+console.log('url :', url)
+if (!url) {
+    throw new Error('❌! Url doesnt found')
+}
+
 export const client = new MongoClient(mongoURI);
 export const runDb = async () => {
     try {
